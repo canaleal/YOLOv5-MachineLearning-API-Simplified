72c1eb30cfc9071ebe41b7ef3ddc7daf3ce04fc7
canaleal <alexcanales766@gmail.com>
2023-01-23 05:11:21 -0500

    Added new input called machine learning stride

--- app/process_video.py
+++ app/process_video.py
@@ -54,44 +54,66 @@ weights = "app/weights/yolov5s.pt"
 
 
 def callSubProcess(command):
-    subprocess.call(command, shell=True)
+    """
+    This function takes in a command and calls it using the subprocess module.
+    :param command: The command to be executed.
+    """
+    try:
+        subprocess.call(command, shell=True)
+    except Exception as e:
+        # Log the error message
+        logging.error(e)
 
 
 def downloadVideoGivenURLAndSaveToPath(url, path):
+    """
+    This function takes in a video URL and a file path, and downloads the video to the specified path.
+    :param url: The URL of the video to be downloaded.
+    :param path: The file path to save the downloaded video.
+    :return: The file path where the video was saved.
+    """
     try:
         urllib.request.urlretrieve(url, path)
         return path
     except Exception as e:
+        # Log the error message
         logging.error(e)
         return e
 
 
-def get_video_for_processing(video_link=''):
+def get_video_for_processing(video_link='', machine_learning_stride=1):
+    """
+    This function takes in a video link and processes it by downloading it, running object detection on it, 
+    and compressing the output video. It returns the path to the compressed output video.
+    :param video_link: The link to the video to be processed.
+    :param machine_learning_stride: The stride to be used for object detection. (Higher stride = faster processing)
+    :return: The path to the compressed output video.
+    """
     try:
-
-        # * Check if folders exist, if not create them
+        # Check if input and output folders exist, if not create them
         checkIfFolderExistsAndCreateIfNot(input_folder_path)
         checkIfFolderExistsAndCreateIfNot(output_folder_path)
 
-        # * Download the video
+        # Download the video
         downloadVideoGivenURLAndSaveToPath(video_link, input_video_path)
 
-        # * Create the subprocess
-        stride = 1
+        # Create the subprocess for object detection
+        stride = machine_learning_stride
         process = f'python {detect_python_path} --weights {weights} --source {input_video_path} --vid-stride {stride} --conf-thres {0.4}'
         callSubProcess(process)
 
-        # * Create ffmpeg process
+        # Create ffmpeg process to compress the output video
         callSubProcess(f'ffmpeg -y -i {output_video_path} -map 0 -c copy {output_video_codec_path}')
         callSubProcess(f'ffmpeg -y -i {output_video_codec_path} -vcodec  libx264  {output_video_codec_compressed_path}')
 
-        # * Delete all files (input, output)
+        # Delete all files (input, output)
         deleteAllFilesInFolder(input_folder_path)
         # deleteAllFilesInFolder(output_folder_path)
 
-        # * Return the path to the new file
+        # Return the path to the new file
         return output_video_codec_compressed_path
 
     except Exception as e:
+        # Log the error message
         logging.error(e)
         return e
--- app/routes/route.py
+++ app/routes/route.py
@@ -4,7 +4,7 @@ import validators
 import app.process_video as process_video
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, validator
-
+import os
 
 router = APIRouter(
     prefix="/dashcam",
@@ -12,9 +12,12 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# * Model for the request body
+
 
 class DashcamVideo(BaseModel):
     video_link: str
+    machine_learning_stride: int = 1
 
     @validator("video_link", pre=True, always=True)
     def check_recording_link(cls, recording_link):
@@ -34,7 +37,13 @@ async def read_root(dashcamVideo: DashcamVideo):
         raise HTTPException(
             status_code=404, detail="Video URL is not valid.")
 
-    output_video_codec_compressed_path = process_video.get_video_for_processing(modelInput['video_link'])
+    output_video_codec_compressed_path = process_video.get_video_for_processing(
+        modelInput['video_link'], modelInput['machine_learning_stride'])
+
+    # Checking if the output video file exists
+    if not os.path.exists(output_video_codec_compressed_path):
+        raise HTTPException(
+            status_code=404, detail="Output video not found.")
 
     def iterfile():
         with open(output_video_codec_compressed_path, mode="rb") as file_like:
